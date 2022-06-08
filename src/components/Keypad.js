46d290f6b85d587a601ab12442a3941e06f7b2e4
Connie Park <conniedc1206@gmail.com>
2022-06-08 08:51:43 -0700

    commit tree dd5af609c406ead1111310d9ce6c9ae7d4cc5dbd

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
-// Code Keypad Component Here
+const Keypad = () => {
+    
+    const handleKeypadChange = () => console.log('Entering password...') 
+
+    return (
+        <input onChange={handleKeypadChange} type="password"></input>
+    )
+}
+
+export default Keypad
